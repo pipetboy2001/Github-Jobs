67d1f12e717bb97d3583f68dd108877efd4b4261
Pipetboy <felipefelizx@gmail.com>
2023-01-18 03:28:40 -0300

    Feature: Creación de un Header

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h1 className='title'>
+            <img src='https://github.com/pipetboy2001/Github-Jobs/blob/main/public/logo192.png?raw=true'
+                className='github-logo' />
+            <b>Github</b> Jobs</h1>
+    )
+}
