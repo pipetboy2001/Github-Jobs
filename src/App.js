66d62b57fefd5cb810609fb2e055e6edf442c027
Pipetboy <felipefelizx@gmail.com>
2023-01-18 04:02:41 -0300

    Feature: Añadir Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import JobSearch from './Components/JobSearch';
 import JobPage from './Components/JobPage';
+import { Footer } from './Components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<JobSearch />} />
         <Route path="/job/:id" element={<JobPage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../Styles/Footer.css';
+
+export const Footer = () => {
+    return (
+        <div className="footer">
+            <p>
+                🖤Created by <a href='https://github.com/pipetboy2001'>Pipetboy</a>🖤
+            </p>
+        </div>
+    );
+};
